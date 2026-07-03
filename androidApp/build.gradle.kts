4e63c488489e001681907c538d7cb70035431148
Milku <rafaeluraev76@gmail.com>
2026-07-04 01:37:27 +0600

    ci: setup automated multiplatform release workflow

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,15 +11,28 @@ android {
         applicationId = "org.milkdev.dreamplayer"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "0.3.0-alpha"
+        versionCode = 1
+        versionName = "0.1.0-alpha1"
+    }
+
+    signingConfigs {
+        create("release") {
+            val keystorePath = System.getenv("KEYSTORE_PATH") ?: "myKey"
+            val keystoreFile = rootProject.file(keystorePath)
+
+            if (keystoreFile.exists()) {
+                storeFile = keystoreFile
+                storePassword = System.getenv("KEYSTORE_PASSWORD") ?: "ТВОЙ_ПАРОЛЬ_ЛОКАЛЬНО"
+                keyAlias = System.getenv("KEY_ALIAS") ?: "key0"
+                keyPassword = System.getenv("KEY_PASSWORD") ?: "ТВОЙ_ПАРОЛЬ_ЛОКАЛЬНО"
+            }
+        }
     }
 
     buildFeatures {
         compose = true
     }
 
-    // Optional but recommended for clean build setups
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -29,6 +42,14 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
+
+            val keystorePath = System.getenv("KEYSTORE_PATH") ?: "myKey"
+            if (rootProject.file(keystorePath).exists()) {
+                signingConfig = signingConfigs.getByName("release")
+            } else {
+                signingConfig = signingConfigs.getByName("debug")
+                logger.warn("Release keystore file not found! Falling back to debug signing configuration.")
+            }
         }
     }
 }
